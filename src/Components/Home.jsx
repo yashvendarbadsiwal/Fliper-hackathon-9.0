5e851e800e0811ddabc5f06475ff371acbcc6e4d
yashvendarbadsiwal <yashalwar4@gmail.com>
2021-06-27 11:25:38 +0530

    getting aks changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,25 +9,30 @@ const mails = new Array(15).fill({
   time: Date(),
 });
 const renderMail = (mail) => {
+  const date = new Date(mail.time);
   return (
-    <div className="shadow mb-3 mt-3 pb-2 pt-2 mail-box">
+    <div className="shadow mb-3 mt-3 pb-2 pt-2 mail-box row">
       <div className="row">
-        <div className="col-12 col-sm-8 col-md-6">
+        <div className="col-12 col-sm-8 col-md-9">
           <h5 className="ml-4">{mail.subject}</h5>
         </div>
-        <div className="col-12 col-sm-3">
-          {mail.schadule && <p>{mail.time}</p>}
+        <div className="col-12 col-sm-3 col-md-2" style={{ opacity: 0.7 }}>
+          {mail.schadule && (
+            <p>{`${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`}</p>
+          )}
         </div>
       </div>
       <div className="row">
         {mail.text && <div>{mail.text}</div>}
         {!mail.text && <div>Html content</div>}
       </div>
-      <div className="align-item-right">
-        <Button outline color="primary pull-left">
-          Edit
-        </Button>
-        <Button color="danger pull-left">Delete</Button>
+      <div className="row">
+        <div className="mail-buttons">
+          <Button outline color="primary">
+            Edit
+          </Button>
+          <Button color="danger">Delete</Button>
+        </div>
       </div>
     </div>
   );
